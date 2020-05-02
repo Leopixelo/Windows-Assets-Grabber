c0bf5226906dbd4abb843a3faeb0cacf0c6bb564
Leonard Gies <kleinerbear2006@gmail.com>
2020-05-02 22:40:52 +0200

    Optimized to not copy every image every time

--- get assets.py
+++ get assets.py
@@ -7,18 +7,21 @@ assets_path = expanduser(
     "~") + "/AppData/Local/Packages/Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy/LocalState/Assets/"
 assets_converted_path = expanduser("~") + "/Pictures/Winodws Assets/Assets Converted/"
 
+assets_converted = listdir(assets_converted_path)
+
 filter_logos = True  # determines whether the program should remove images that look like logos
 filter_portrait_pictures = False  # determines whether the program should remove portrait images
 
 for f in listdir(assets_path):
-    copy(assets_path + f, assets_converted_path + f + ".png")
-    if filter_logos or filter_portrait_pictures:
-        img = PIL_open(assets_converted_path + f + ".png")
-        size = img.size
-        img.close()
+    if f + ".png" not in assets_converted:
+        copy(assets_path + f, assets_converted_path + f + ".png")
+        if filter_logos or filter_portrait_pictures:
+            img = PIL_open(assets_converted_path + f + ".png")
+            size = img.size
+            img.close()
 
-        if filter_logos and size[0] == size[1]:  # if the image is square its likely to be a logo
-            remove(assets_converted_path + f + ".png")
+            if filter_logos and size[0] == size[1]:  # if the image is square its likely to be a logo
+                remove(assets_converted_path + f + ".png")
 
-        if filter_portrait_pictures and size[0] < size[1]:
-            remove(assets_converted_path + f + ".png")
+            if filter_portrait_pictures and size[0] < size[1]:
+                remove(assets_converted_path + f + ".png")
